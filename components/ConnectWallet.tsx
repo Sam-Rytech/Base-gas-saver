44f191513bb19815ca6844031efdf887d5e5ee6f
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-07 00:41:22 +0100

    Wallet connect component

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
+import { ethers } from 'ethers'
 
-const ConnectWallet = () => {
-  return (
-    <div>ConnectWallet</div>
-  )
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
 }
 
-export default ConnectWallet
+export default function ConnectWallet({
+  onConnect,
+}: {
+  onConnect: (address: string, signer: any) => void
+}) {
+  const [address, setAddress] = useState('')
+
+  const connect = async () => {
+    if (!window.ethereum) return alert('Install MetaMask!')
+
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const accounts = await provider.send('eth_requestAccounts', [])
+    const signer = await provider.getSigner()
+    const userAddress = accounts[0]
+
+    setAddress(userAddress)
+    onConnect(userAddress, signer)
+  }
+
+  return (
+    <button
+      onClick={connect}
+      className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700"
+    >
+      {address ? `Connected: ${address.slice(0, 6)}...` : 'Connect Wallet'}
+    </button>
+  )
+}
