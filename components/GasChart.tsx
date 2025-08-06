eaf98b978665bc7c4138462982eafb0ac96b363b
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-07 00:53:22 +0100

    Added Gascharts component

--- components/GasChart.tsx
+++ components/GasChart.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+'use client'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 
-const GasChart = () => {
+export default function GasChart({
+  data,
+}: {
+  data: { time: string; price: number }[]
+}) {
   return (
-    <div>GasChart</div>
+    <div className="h-64 mt-6">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={data}>
+          <XAxis dataKey="time" hide />
+          <YAxis domain={['auto', 'auto']} unit=" gwei" />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="price"
+            stroke="#3b82f6"
+            strokeWidth={2}
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
-
-export default GasChart
+""
