ec46438607368fa28a430ef0ac640ee21ddce7be
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-07 00:19:00 +0100

    initialized all files needed

---
+++ components/GasChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GasChart = () => {
+  return (
+    <div>GasChart</div>
+  )
+}
+
+export default GasChart
---
+++ components/GasTracker.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GasTracker = () => {
+  return (
+    <div>GasTracker</div>
+  )
+}
+
+export default GasTracker
